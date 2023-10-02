53fea6923b68a104b8b558afbb0f656e4349112b
Brandon Kammerdiener <kammerdienerb@kammerdienerbm1max.nomads.utk.edu>
2023-10-02 09:49:47 -0400

    highlight request

--- sh.c
+++ sh.c
@@ -66,6 +66,12 @@ void eline(yed_event *event)  {
 
     yed_syntax_line_event(&syn, event);
 }
+void ehigh(yed_event *event)  {
+    if (event->ft == yed_get_ft("Shell")) {
+        yed_syntax_highlight_request_event(&syn, event);
+        event->cancel = 1;
+    }
+}
 
 
 void unload(yed_plugin *self) {
@@ -77,6 +83,7 @@ int yed_plugin_boot(yed_plugin *self) {
     yed_event_handler buffdel;
     yed_event_handler buffmod;
     yed_event_handler line;
+    yed_event_handler high;
 
     char              *kwds[] = {
         "time", "shift",
@@ -118,6 +125,10 @@ int yed_plugin_boot(yed_plugin *self) {
     line.kind = EVENT_LINE_PRE_DRAW;
     line.fn   = eline;
     yed_plugin_add_event_handler(self, line);
+
+    high.kind = EVENT_HIGHLIGHT_REQUEST;
+    high.fn   = ehigh;
+    yed_plugin_add_event_handler(self, high);
 
 
     SYN();
